884caec202bd48800a8557cd991bc442ea30242e
丁明信 <dingmingxin20@gmail.com>
2015-04-01 00:05:58 +0800

    remove some files; add new files

---
+++ config_bash/bash_utils.sh
@@ -0,0 +1,33 @@
+#!/bin/bash - 
+#===============================================================================
+#
+#          FILE: bash_utils.sh
+# 
+#         USAGE: ./bash_utils.sh 
+# 
+#   DESCRIPTION: 
+# 
+#       OPTIONS: ---
+#  REQUIREMENTS: ---
+#          BUGS: ---
+#         NOTES: ---
+#        AUTHOR: dingmingxin
+#  ORGANIZATION: 
+#       CREATED: 03/31/2015 23:57
+#      REVISION:  ---
+#===============================================================================
+
+# dash search
+search_dash()
+{
+	case $# in
+		0) open dash://
+			;;
+		1) open dash://${1}
+			;;
+		2) open dash://${1}:${2}
+			;;
+		*) open dash://
+			;;
+	esac
+}
