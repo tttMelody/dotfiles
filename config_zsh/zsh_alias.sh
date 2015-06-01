2144a52b276af159c662961bb5bced3fb576d3f7
丁明信 <dingmingxin@babeltime.com>
2015-06-01 10:06:58 +0800

    add zsh config

---
+++ config_zsh/zsh_alias.sh
@@ -0,0 +1,38 @@
+#!/bin/bash - 
+#===============================================================================
+#
+#          FILE: bash_aliases_usefull.sh
+# 
+#         USAGE: ./bash_aliases_usefull.sh 
+# 
+#   DESCRIPTION: 
+# 
+#       OPTIONS: ---
+#  REQUIREMENTS: ---
+#          BUGS: ---
+#         NOTES: ---
+#        AUTHOR: dingmingxin
+#  ORGANIZATION: 
+#       CREATED: 03/31/2015 23:57
+#      REVISION:  ---
+#===============================================================================
+unamestr=$(uname)
+platform="unknown"
+
+alias svnci='svnrmall; svnaddall; svn ci'
+alias hg='history | grep '
+alias la="ls -lhG"
+alias ll='ls -dltr ^*.(meta|csproj|userprefs|sln|unityproj|csproj)'
+alias ld='ll|grep "^d"'
+alias op='open'
+alias bs='source ~/.bash_profile'
+
+if [[ $unamestr == "Darwin" ]]; then
+	platform="osx"
+elif [[ $unamestr == "Linux" ]]; then
+	platform="linux"
+fi
+
+if [[ $platform == "osx" ]]; then
+	alias ds='search_dash'
+fi
---
+++ config_zsh/zsh_custom/zsh_alias.sh
@@ -0,0 +1,39 @@
+#common
+alias svnci='svnrmall; svnaddall; svn ci'
+alias hg='history | grep '
+alias op='open'
+
+#sanguo2
+alias s2root='cd /data/sanguo2'
+alias s2lua='cd /data/sanguo2/script'
+alias s2pj='cd /data/sanguo2/client'
+alias s2svn='cd /data/sanguo2/svn'
+alias s2ui='cd /data/sanguo2/program/Assets/anim/ui'
+alias s2config='cd /data/sanguo2/program/config'
+alias s2expimage='python /data/sanguo2/client/tool.py tp2 q'
+alias s2expconfig='python /data/sanguo2/client/tool.py cfg q'
+alias s2php='cd /data/wwwroot/sanguo2/apps/mingxin'
+alias s2upanim='svn up /data/sanguo2/program/Assets/anim'
+alias s2upconfig='svn up /data/sanguo2/program/config'
+alias s2phpconfig='cd /data/sanguo2/tmp/package/phpconfig/'
+
+#wtcg
+alias wasset="cd /data/wtcg/client/Assets/"
+alias ws="cd /data/wtcg/client/Assets/Script/"
+alias wclient="cd /data/wtcg/client/"
+alias wlua="cd /data/wtcg/client/Assets/Slua/Resources/"
+alias wexpconfig="python /data/wtcg/client/tool.py cfg q"
+alias whost="ssh pirate@192.168.1.181"
+alias wconfig="cd /data/wtcg/client/Assets/Resources/config"
+alias wserver='cd /data/wtcg/server/'
+
+#raid
+alias rclient="cd /data/raid/client/"
+alias rlua="cd /data/raid/client/Assets/Slua/Resources"
+alias rserver="cd /data/raid/server"
+
+#ssh
+alias dphoho='ssh deploy@hohoplay.com'
+
+
+
