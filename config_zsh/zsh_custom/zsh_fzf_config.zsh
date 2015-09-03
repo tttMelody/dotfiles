63425c043284d2a1a46f91ba510404f029c3ae73
丁明信 <dingmingxin20@gmail.com>
2015-09-03 20:36:45 +0800

    增加fzf 函数

---
+++ config_zsh/zsh_custom/zsh_fzf_config.zsh
@@ -0,0 +1,6 @@
+export FZF_DEFAULT_OPTS='
+--extended
+--bind ctrl-f:page-down,ctrl-b:page-up
+--color fg:252,bg:233,hl:67,fg+:252,bg+:235,hl+:81
+--color info:144,prompt:161,spinner:135,pointer:135,marker:118
+'
