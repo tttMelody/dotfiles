01327e8e63c406a977c883721cab186f2284b25b
丁明信 <dingmingxin20@gmail.com>
2015-09-03 19:13:59 +0800

    add some fzf functions

--- config_zsh/zsh_custom/zsh_funcs.zsh
+++ config_zsh/zsh_custom/zsh_funcs.zsh
@@ -1,3 +1,4 @@
+# for tmux select-pane
 ftpane () {
 	local panes current_window target target_window target_pane
 	panes=$(tmux list-panes -s -F '#I:#P - #{pane_current_path} #{pane_current_command}')
@@ -16,3 +17,34 @@ ftpane () {
 	fi
 }
 
+#for tmux select session
+fs() {
+	local session
+	session=$(tmux list-sessions -F "#{session_name}" | \
+		fzf --query="$1" --select-1 --exit-0) &&
+		tmux switch-client -t "$session"
+}
+#change directory
+fd() {
+	local dir
+	dir=$(find ${1:-*} -path '*/\.*' -prune \
+		-o -type d -print 2> /dev/null | fzf +m) &&
+		cd "$dir"
+}
+
+#change directory including hidden directory
+fda() {
+	local dir
+	dir=$(find ${1:-.} -type d 2> /dev/null | fzf +m) && cd "$dir"
+}
+
+# cdf - cd into the directory of the selected file
+cdf() {
+	local file
+	local dir
+	file=$(fzf +m -q "$1") && dir=$(dirname "$file") && cd "$dir"
+}
+
+fh() {
+	eval $( ([ -n "$ZSH_NAME" ] && fc -l 1 || history) | fzf +s --tac | sed 's/ *[0-9]* *//')
+}
