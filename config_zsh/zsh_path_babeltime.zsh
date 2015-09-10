b0b1781fb9157bc9ea53265d18ec24c6b7610afa
丁明信 <dingmingxin@babeltime.com>
2015-09-10 14:14:48 +0800

    add path config

---
+++ config_zsh/zsh_path_babeltime.zsh
@@ -0,0 +1,15 @@
+export COCOS_CONSOLE_ROOT=/data/cocos/tools/cocos2d-console/bin
+export COCOS_TEMPLATES_ROOT=/data/cocos/templates
+export NDK_ROOT=/data/android/android-ndk-r10e
+export ANDROID_SDK_ROOT=/data/android/android-sdk-macosx
+export ANT_ROOT=/opt/local/share/java/apache-ant/bin
+#path+=( ~/bin /opt/local/bin $ANT_ROOT $COCOS_CONSOLE_ROOT ~/gocode/bin /opt/local/lib/mysql55/bin ~/.fzf/bin )
+export PATH=/usr/local/bin:/opt/local/bin:$PATH
+export PATH=/usr/local/go/bin:$PATH
+export PATH=/opt/local/lib/mysql55/bin:$PATH
+export PATH=$HOME/bin:$PATH
+export PATH=~/.fzf/bin:$PATH
+export PATH=~/gocode/bin:$PATH
+export PATH=$ANT_ROOT:$COCOS_CONSOLE_ROOT:$ANDROID_SDK_ROOT:$PATH
+export MANPATH=/opt/local/share/man:$MANPATH
+
