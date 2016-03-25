5fb23ac138f6532e3062d410d4480d9977e91b52
dingmingxin <dingmingxin20@gmail.com>
2016-03-26 02:02:50 +0800

    fix add hook error

--- config_spacemacs/layers/dmx/funcs.el
+++ config_spacemacs/layers/dmx/funcs.el
@@ -13,11 +13,10 @@
 						(linum-relative-toggle))
 
   ;; org mode 禁用行号
-  (add-hook 'org-mode-hook ((lambda ()
+  (add-hook 'org-mode-hook (lambda ()
 							  "disable linum in org mode"
-							  (linum-mode 0))))
+							  (linum-mode 0)))
   ;; helm buffer 禁用行号
   (add-hook 'helm-after-initialize-hook (lambda ()
 										  (with-helm-buffer
-											(linum-mode 0))))
-  )
+											(linum-mode 0)))))
