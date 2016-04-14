0e4bd3363ca3c573d572bad66eb5dc3189516005
dingmingxin <dingmingxin20@gmail.com>
2016-04-14 08:35:11 +0800

    update spacemacs config; update tmux mouse config

--- config_spacemacs/init.el
+++ config_spacemacs/init.el
@@ -46,6 +46,7 @@ values."
      tmux
      games
      dash
+	 shell-scripts
      ;;chinese
      ;; (colors :variables
      ;;         colors-enable-rainbow-identifiers t
@@ -291,6 +292,14 @@ user code."
   (c-set-offset 'substatement-open '0)
   (add-hook 'c++-mode-hook 'dmx-c-mode-hook)
   (add-hook 'c-mode-hook 'dmx-c-mode-hook)
+  ;; 禁用鼠标
+  (xterm-mouse-mode -1)
+  (dolist (k '([mouse-1] [down-mouse-1] [drag-mouse-1] [double-mouse-1] [triple-mouse-1]
+			   [mouse-2] [down-mouse-2] [drag-mouse-2] [double-mouse-2] [triple-mouse-2]
+			   [mouse-3] [down-mouse-3] [drag-mouse-3] [double-mouse-3] [triple-mouse-3]
+			   [mouse-4] [down-mouse-4] [drag-mouse-4] [double-mouse-4] [triple-mouse-4]
+			   [mouse-5] [down-mouse-5] [drag-mouse-5] [double-mouse-5] [triple-mouse-5]))
+	  (global-unset-key k))
   (dmx/linum-config)
   )
 
