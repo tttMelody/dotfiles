e60e5a0f394e31e775b1afeca73637e53c4fcccf
dmx <dingmingxin20@gmail.com>
2016-12-09 00:15:01 +0800

    update emacs config

--- config_spacemacs/layers/dmx/packages.el
+++ config_spacemacs/layers/dmx/packages.el
@@ -28,7 +28,7 @@
 		chinese-fonts-setup
 		slime
 		rainbow-mode
-		osx-clipboard
+		;; osx-clipboard
 		))
 
 ;; List of packages to exclude.
@@ -193,11 +193,11 @@
 	(progn
 	 (require 'ob-lua)
 	  (message "Loading %s..." "ob-lua"))))
-(defun dmx/init-osx-clipboard()
-  "Initialize osx-clipboard"
-  (use-package osx-clipboard
-	:config
-	(progn
-	  (osx-clipboard-mode +1)
-	  (diminish 'osx-clipboard-mode))))
+;; (defun dmx/init-osx-clipboard()
+;;   "Initialize osx-clipboard"
+;;   (use-package osx-clipboard
+;; 	:config
+;; 	(progn
+;; 	  (osx-clipboard-mode +1)
+;; 	  (diminish 'osx-clipboard-mode))))
 
